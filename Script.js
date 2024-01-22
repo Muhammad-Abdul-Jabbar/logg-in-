03ca5f7864748c982a3edaac5b721d2d72662d3c
Abdul Jabbar <122173146+Muhammad-Abdul-Jabbar@users.noreply.github.com>
2024-01-22 20:54:28 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,17 @@
+
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errorMessage = document.getElementById("error-message");
+
+    // Legg til din autentiseringslogikk her
+    // For enkelhet antar vi brukernavnet "user" og passordet "pass"
+    if (username === "user" && password === "pass") {
+        // Login vellykket, omdiriger til en annen side
+        window.location.href = "hjem.html";
+    } else {
+        errorMessage.innerHTML = "Feil brukernavn eller passord. Prøv igjen.";
+        errorMessage.style.color = "red";
+    }
+}
